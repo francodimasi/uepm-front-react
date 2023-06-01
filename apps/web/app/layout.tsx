5808ab35b9589bb0330f07c2917fdfbca1c61244
Felipe Nunes <fdesouza@mobilecomputing.com.ar>
2023-06-01 14:51:27 -0300

    add Review component

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,5 @@
 import "ui/styles.css";
+import './globals.css';
 
 export default function RootLayout({
   children,
--- apps/web/app/partners/page.tsx
+++ apps/web/app/partners/page.tsx
@@ -1,11 +1,12 @@
 import { Header } from "ui";
 import { Layout } from '../../components/core/layout/Layout';
+import { Reviews } from './components/Reviews';
 
 export default function Page() {
   return (
     <Layout>
-      <Header text="Partners" />
-      <p>This is the Partners list</p>
+      {/* <Header text="Partners" /> */}
+      <Reviews />
     </Layout>
   );
 }
