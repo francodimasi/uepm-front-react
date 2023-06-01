136dc829d5370329fcdb35a9e844d3ccfa1fc7c5
Lucas <libattistella@gmail.com>
2023-06-01 18:46:22 +0200

    feat: FE-18 | Header

---
+++ packages/ui/core/navigation/NavLink.types.ts
@@ -0,0 +1,9 @@
+import { PropsWithClassName } from "../../types/core";
+
+export type NavLinkProps = PropsWithClassName & {
+  label: string;
+  href: string;
+  hovered: boolean;
+  onMouseEnter: () => void;
+  onMouseLeave: () => void;
+};
