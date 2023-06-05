a49c0877f79d0fe3fe76d177ede36e4b00e8fd8a
Felipe Nunes <fdesouza@mobilecomputing.com.ar>
2023-06-05 17:23:34 -0300

    create partner summary

---
+++ apps/web/app/partners/components/PartnersSummary.tsx
@@ -0,0 +1,21 @@
+
+export const PartnersSummary = () => {
+  return (
+    <section
+      aria-labelledby="reviews-title"
+      className="pt-20 sm:pt-32 px-4"
+    >
+      <h2 className="text-3xl font-medium tracking-tight text-gray-900 sm:text-center">
+        Nos acompañan
+      </h2>
+      <div className="flex items-center justify-center">
+        <p className="mt-2 md:w-3/5 justify-self-center text-lg text-gray-600 sm:text-center">
+          Las siguientes Asociaciones de Pacientes y Fundaciones nos acompañan
+          en este proyecto de acercarte los ensayos clínicos en los que se está
+          trabajando para que los investigadores puedan descubrir nuevos
+          tratamientos para tu enfermedad.
+        </p>
+      </div>
+    </section>
+  );
+};
