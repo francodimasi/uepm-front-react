84cdaaa565dd6183e70347734287a514bbf928da
Felipe Nunes <fdesouza@mobilecomputing.com.ar>
2023-06-08 13:18:05 -0300

    add custom provider component

--- apps/web/app/[lang]/layout.tsx
+++ apps/web/app/[lang]/layout.tsx
@@ -1,15 +1,20 @@
 import "ui/styles.css";
+import { Providers } from "../../core/Providers";
+import { dir } from "i18next";
 
 export default function RootLayout({
   children,
-  params
+  params,
 }: {
   children: React.ReactNode;
-  params: {lang: string}
+  params: { lang: string };
 }) {
+  const { lang } = params;
   return (
-    <html lang={params.lang}>
-      <body>{children}</body>
+    <html lang={lang} dir={dir(lang)}>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
