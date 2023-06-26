11737133724a334aea58c77f25dd85f2358feb40
Felipe Nunes <fdesouza@mobilecomputing.com.ar>
2023-06-26 12:17:27 -0300

    add blog types

---
+++ apps/web/api/blog/yoast.types.ts
@@ -0,0 +1,53 @@
+export type YoastHeadJson = {
+  title: string;
+  robots: YoastRobots;
+  og_locale: string;
+  og_type: string;
+  og_title: string;
+  og_url: string;
+  og_site_name: string;
+  twitter_card: string;
+  schema: YoastSchema;
+};
+
+type YoastRobots = {
+  index: string;
+  follow: string;
+  "max-snippet": string;
+  "max-image-preview": string;
+  "max-video-preview": string;
+};
+
+type YoastSchema = {
+  "@context": string;
+  "@graph": YoastSchemaGraph[];
+};
+
+type YoastSchemaGraph = {
+  "@type": string;
+  "@id": string;
+  url?: string;
+  name?: string;
+  description?: string;
+  isPartOf?: { "@id": string };
+  breadcrumb?: { "@id": string };
+  inLanguage?: string;
+  itemListElement?: YoastSchemaGraphElement[];
+  potentialAction?: YoastSchemaGraphAction[];
+};
+
+type YoastSchemaGraphElement = {
+  "@type": string;
+  position: number;
+  name: string;
+  item?: string;
+};
+
+type YoastSchemaGraphAction = {
+  "@type": string;
+  target: {
+    "@type": string;
+    urlTemplate: string;
+  };
+  "query-input": string;
+};
