8855523100a702cad7fed6a9a6473488bd835c82
felipenunesdev <68870005+felipenunesdev@users.noreply.github.com>
2023-08-29 14:17:48 -0300

    create cover component

---
+++ apps/landing/app/components/cover/Cover.tsx
@@ -0,0 +1,13 @@
+export const Cover = () => {
+    
+  return (
+    <section className="bg-dark">
+      <div className="container mx-auto">
+        <h1 className="font-sans">
+          Innovación y tecnología para revolucionar el reclutamiento de
+          pacientes en Latinoamérica
+        </h1>
+      </div>
+    </section>
+  );
+};
--- apps/landing/app/page.tsx
+++ apps/landing/app/page.tsx
@@ -1,9 +1,11 @@
+import { Cover } from "./components/cover/Cover";
 import { Header } from "./components/header/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Cover />
     </>
   );
 }
