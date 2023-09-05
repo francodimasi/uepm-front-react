c8ab8c67b245cc146e706cae2f7f14661599efb1
felipenunesdev <felipenunesdev@gmail.com>
2023-09-05 20:42:27 -0300

    Clone landing onco

---
+++ apps/landing-onco/app/components/cover/components/Molecules.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { forwardRef } from "react";
+
+import Image from "next/image";
+import elipses from "public/images/cover/elipses.png";
+
+export const Molecules = forwardRef<any, any>((props, ref) => {
+  return (
+    <div className="h-full w-full absolute top-0 -right-6 overflow-hidden">
+      <Image
+        ref={ref}
+        className="absolute top-0 right-0 w-5/12"
+        src={elipses}
+        alt="elipses"
+      />
+    </div>
+  );
+});
