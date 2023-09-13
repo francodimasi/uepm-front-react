bbbb1b66017f798cd6c52c4bb41e4e7bf1e181db
Felipe Nunes <felipenunesdev@gmail.com>
2023-09-13 20:21:48 -0300

    add selector to svg map

--- apps/landing/app/components/sites/components/Map.tsx
+++ apps/landing/app/components/sites/components/Map.tsx
@@ -1,13 +1,33 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { SvgLoader, SvgProxy } from "react-svgmt";
 
+const selectors = [
+  "argentina",
+  "brasil",
+  "chile",
+  "colombia",
+  "peru",
+  "mexico",
+];
+
 export const Map = ({ className }: React.HTMLAttributes<HTMLDivElement>) => {
+  const [selected, setSelected] = useState(selectors[0]);
+
   return (
     <div className={`${className}`}>
       <SvgLoader path="images/sites/map.svg">
-
+        {selectors.map((selector, index) => (
+          <SvgProxy
+            key={index}
+            onClick={() => setSelected(selector)}
+            selector={`#${selector}`}
+            class={`map-marker ${
+              selected === selector ? "map-marker-selected" : ""
+            }`}
+          />
+        ))}
       </SvgLoader>
     </div>
   );
