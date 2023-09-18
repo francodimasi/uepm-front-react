71f3aead9b5cdf5bda55b14428d37bca317abc4d
Felipe Nunes <felipenunesdev@gmail.com>
2023-09-18 12:38:45 -0300

    remove useMemo

--- apps/landing/app/components/shared/counter/CounterWrapper.tsx
+++ apps/landing/app/components/shared/counter/CounterWrapper.tsx
@@ -13,40 +13,40 @@ export const CounterWrapper = ({
   specialties,
 }: Site) => {
   const { selected } = useContext(SitesContext);
-  const counterData: CounterItem[] = useMemo(
-    () => [
-      {
-        label: "Especialistas",
-        value: specialists,
-      },
-      {
-        label: "Sedes Clínicas",
-        value: clinics,
-      },
-      {
-        label: "Ciudades",
-        value: cities,
-      },
-      {
-        label: "Especialidades",
-        value: specialties,
-      },
-    ],
-    [id]
+  const counterData: CounterItem[] = [
+    {
+      label: "Especialistas",
+      value: specialists,
+    },
+    {
+      label: "Sedes Clínicas",
+      value: clinics,
+    },
+    {
+      label: "Ciudades",
+      value: cities,
+    },
+    {
+      label: "Especialidades",
+      value: specialties,
+    },
+  ];
+
+  const CounterList = useCallback(
+    () => (
+      <>
+        <h3 className="text-light font-bold text-base sm:text-2xl">{name}</h3>
+        <div className="grid  grid-cols-2 sm:grid-cols-4 mt-2.5 sm:mt-10">
+          {counterData.map((item, index) => (
+            <div key={index} className="my-5 sm:my-0 col-span-1">
+              <Counter {...item} />
+            </div>
+          ))}
+        </div>
+      </>
+    ),
+    [selected]
   );
 
-  const CounterList = useCallback(() => (
-    <>
-      <h3 className="text-light font-bold text-base sm:text-2xl">{name}</h3>
-      <div className="grid  grid-cols-2 sm:grid-cols-4 mt-2.5 sm:mt-10">
-        {counterData.map((item, index) => (
-          <div key={index} className="my-5 sm:my-0 col-span-1">
-            <Counter {...item} />
-          </div>
-        ))}
-      </div>
-    </>
-  ), [selected]);
-
-  return <CounterList />
+  return <CounterList />;
 };
