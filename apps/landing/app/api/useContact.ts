8c3e6f3f0c18fc20fea0de838c36645f8c77ced9
Felipe Nunes <felipenunesdev@gmail.com>
2023-09-29 13:50:50 -0300

    fix monday

--- apps/landing/app/api/useContact.ts
+++ apps/landing/app/api/useContact.ts
@@ -35,7 +35,6 @@ export const useContact = () => {
             }
         `.replace(/(\r\n|\n|\r)/gm, "");
 
-    console.log(query);
     return post("https://api.monday.com/v2", {
       customToken: mondayApiKey,
       data: {
