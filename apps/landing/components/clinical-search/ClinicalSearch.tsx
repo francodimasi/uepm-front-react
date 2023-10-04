21599aa89ff49ccea04fa4fa7bd966a8cf4f9506
Felipe Nunes <felipenunesdev@gmail.com>
2023-10-03 21:36:42 -0300

    add translations to clinical search section

--- apps/landing/components/clinical-search/ClinicalSearch.tsx
+++ apps/landing/components/clinical-search/ClinicalSearch.tsx
@@ -1,26 +1,30 @@
 "use client";
 
+import { useContext } from 'react';
 import { openUrl } from "utils";
 import { LandingButton } from "ui";
 import { LINKS } from "@constants/links.const";
+import { LanguageContext, useClientTranslation } from 'i18n';
 
 
 export const ClinicalSearch = () => {
+  const { lang } = useContext(LanguageContext)
+  const { t } = useClientTranslation(lang, { keyPrefix: "clinicalSearch" });
+
   return (
     <section className="pb-32">
       <div className="container xl:px-28 2xl:px-48">
         <div className="lg:grid grid-cols-12 p-8 sm:p-16 bg-gradient-to-br to-primary from-primary-dark">
-          <p className="text-light text-2xl col-span-9">
-            Descubre el primer y único buscador online de ensayos  clínicos con
-            lenguaje amigable para Latinoamérica.
+          <p className="text-light text-2xl col-span-9 md:pr-4">
+            {t('text')}
           </p>
           <div className="col-span-3 mt-8 lg:mt-0 flex lg:justify-end items-center">
             <LandingButton
               outlined
-              className="w-full sm:w-auto"
+              className="w-full sm:w-auto whitespace-nowrap"
               onClick={() => openUrl(LINKS.oncoLogin)}
             >
-              Un&nbsp;Ensayo&nbsp;Para&nbsp;Mí
+              {t('button')}
             </LandingButton>
           </div>
         </div>
