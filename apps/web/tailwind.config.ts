b365f2688d1eb02f16d1323f02c25c53eba7b7f7
Felipe Nunes <felipenunesdev@gmail.com>
2023-10-11 09:29:41 -0300

    set configs

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+import tailwindDefault from "configs/tailwind/tailwind.config";
+
+const config: Partial<Config> = {
+  presets: [tailwindDefault],
+  theme: {
+    extend: {
+    },
+  },
+};
+
+
+export default config;
