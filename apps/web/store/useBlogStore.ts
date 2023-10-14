4f7947ec68d297ceb12f70302ec17909650ebab8
Felipe Nunes <felipenunesdev@gmail.com>
2023-10-14 12:21:27 -0300

    create blog store

---
+++ apps/web/store/useBlogStore.ts
@@ -0,0 +1,27 @@
+"use client";
+
+import { create } from "zustand";
+
+type BlogState = {
+  category: number;
+};
+
+type BlogActions = {
+  setCategory: (category: number) => void;
+};
+
+export type BlogStore = BlogActions & BlogState;
+
+const initialState: BlogState = {
+  category: undefined,
+};
+
+export const useBlogStore = create<BlogStore, any>((set) => ({
+  ...initialState,
+  setCategory: (category) => {
+    set(() => ({ category }));
+  },
+  reset: () => {
+    set(initialState);
+  },
+}));
