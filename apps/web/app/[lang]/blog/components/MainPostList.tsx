3c0eac883237f3209594e232fb0dc3590d4bd3e0
Felipe Nunes <felipenunesdev@gmail.com>
2023-10-17 12:57:08 -0300

    fix calls to API when change category tab

--- apps/web/app/[lang]/blog/components/MainPostList.tsx
+++ apps/web/app/[lang]/blog/components/MainPostList.tsx
@@ -1,31 +1,25 @@
 import { Pagination } from "@components/pagination/Pagination"
+import { useBlogStore } from "@store/useBlogStore"
 import { PostList } from "./PostList"
 import { PostListHeader } from "./PostListHeader"
-import { useBlogStore } from "@store/useBlogStore"
-import { useCallback } from "react"
 
 export const MainPostList = () => {
 
     const { category } = useBlogStore();
 
-    const PostListPagination = useCallback(() => {
-        if (!category) return <></>;
-        return (
-            <>
-                <PostList
-                    queryKey="blog_post_list"
-                    filter={{ page: 1, per_page: 6, categories: [category] }}
-                />
-                <Pagination />
-            </>
-        )
-    }, [category])
-
     return (
         <>
-            <div className="py-12">
+            <div className="relative">
                 <PostListHeader />
-                <PostListPagination />
+                {category &&
+                    <>
+                        <PostList
+                            queryKey="blog_post_list"
+                            filter={{ page: 1, per_page: 6, categories: [category] }}
+                        />
+                        <Pagination />
+                    </>
+                }
             </div>
         </>
     )
