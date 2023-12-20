15434dd38a0aa8799f64f62fbe12f00df97f24e2
Lucas <libattistella@gmail.com>
2023-12-20 13:02:01 +0100

    FE-110 | Add use client

--- apps/web/app/[lang]/page.tsx
+++ apps/web/app/[lang]/page.tsx
@@ -1,24 +1,5 @@
-'use client';
-
-import { useLayoutEffect } from 'react';
 import { Layout } from '../../components/core/layout/Layout';
-import { errorResponseHandler } from '@core/error-handler';
-import { useSite } from '@api/site/useSite';
 
 export default function Page() {
-  const { getSites } = useSite();
-
-  const loadSites = async () => {
-    try {
-      await getSites();
-    } catch (e) {
-      errorResponseHandler(e);
-    }
-  };
-
-  useLayoutEffect(() => {
-    loadSites();
-  }, []);
-
   return <Layout></Layout>;
 }
