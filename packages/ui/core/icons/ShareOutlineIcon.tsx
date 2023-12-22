6e6601c43ba3f3dac22284e4a1f2c4ba9a5cb557
Javier Bianciotto <javierbianciotto@gmail.com>
2023-12-22 11:50:07 -0300

    Icons migration to components

---
+++ packages/ui/core/icons/ShareOutlineIcon.tsx
@@ -0,0 +1,15 @@
+export const ShareOutlineIcon = (props: any) => {
+    return (
+<svg width="24" height="26" viewBox="0 0 24 26" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+<g id="vuesax/linear/search-normal">
+<g id="search-normal">
+<path id="Vector" d="M4.5 16.2998C6.433 16.2998 8 14.7328 8 12.7998C8 10.8668 6.433 9.2998 4.5 9.2998C2.567 9.2998 1 10.8668 1 12.7998C1 14.7328 2.567 16.2998 4.5 16.2998Z" stroke="#020001" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+<path id="Vector_2" d="M19.5 24.2998C21.433 24.2998 23 22.7328 23 20.7998C23 18.8668 21.433 17.2998 19.5 17.2998C17.567 17.2998 16 18.8668 16 20.7998C16 22.7328 17.567 24.2998 19.5 24.2998Z" stroke="#020001" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+<path id="Vector_3" d="M19.5 8.2998C21.433 8.2998 23 6.7328 23 4.7998C23 2.86681 21.433 1.2998 19.5 1.2998C17.567 1.2998 16 2.86681 16 4.7998C16 6.7328 17.567 8.2998 19.5 8.2998Z" stroke="#020001" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+<path id="Vector_4" d="M16 19.7998L7.5 15.2998" stroke="#020001" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+<path id="Vector_5" d="M16 6.2998L7.5 10.7998" stroke="#020001" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+</g>
+</g>
+</svg>
+);
+};
