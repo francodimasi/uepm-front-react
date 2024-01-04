5a89ed08108e80f5e54e630084eac980928cbbc8
Lucas Martinez <lsmartinez88@hotmail.com>
2024-01-04 15:47:28 -0300

    styles

--- packages/ui/core/tag/Tag.tsx
+++ packages/ui/core/tag/Tag.tsx
@@ -3,14 +3,11 @@ import { TagProps } from './Tag.types';
 export const Tag: React.FC<TagProps> = ({
   text,
   onClickHandler,
-  className
+  className,
 }) => {
   return (
-    <div
-      className={className}
-      onClick={onClickHandler}
-    >
-      <span className="mx-1.5 sm:mx-1" >{text}</span>
+    <div className={className} onClick={onClickHandler}>
+      <span className="mx-1.5 sm:mx-1">{text}</span>
     </div>
   );
 };
