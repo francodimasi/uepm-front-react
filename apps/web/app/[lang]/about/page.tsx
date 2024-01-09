18ec72cab7374da11d353e125b110e378f19f799
Lucas <libattistella@gmail.com>
2024-01-09 22:33:34 +0100

    FE-106 | Partners

--- apps/web/app/[lang]/about/page.tsx
+++ apps/web/app/[lang]/about/page.tsx
@@ -1,5 +1,6 @@
 import { Layout } from '@components/core/layout/Layout';
-import { Team } from './components/team/Team';
+import { Team } from './components/team';
+import { Partners } from './components/partners';
 import { promises as fs } from 'fs';
 
 export default async function Page({ params: { lang } }) {
@@ -14,11 +15,21 @@ export default async function Page({ params: { lang } }) {
     return JSON.parse(team);
   };
 
+  const getPartners = async () => {
+    const partners = await fs.readFile(
+      process.cwd() + '/api/mocks/ourPartners.json',
+      'utf8',
+    );
+    return JSON.parse(partners);
+  };
+
   const team = await getTeam();
+  const partners = await getPartners();
 
   return (
     <Layout locale={lang}>
       <Team board={team.board} staff={team.staff} />
+      <Partners partners={partners} />
     </Layout>
   );
 }
---
+++ packages/ui/core/scroll/Scroll.types.ts
@@ -0,0 +1,5 @@
+import { PropsWithClassName } from '../../types/core';
+
+export type ScrollProps = PropsWithClassName & {
+  orientation?: 'hozizontal' | 'vertical';
+};
