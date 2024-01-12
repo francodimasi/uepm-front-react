d1b455a971b040a8092dfd833245673c17c9c225
Lucas <libattistella@gmail.com>
2024-01-12 16:09:00 +0100

    FE-129 | Entend Button component

--- packages/ui/core/button/helpers.ts
+++ packages/ui/core/button/helpers.ts
@@ -1,7 +1,27 @@
 import clsx from 'clsx';
-import { ButtonColor, ButtonSize, ButtonVariant } from './Button.types';
+import {
+  ButtonColor,
+  ButtonFill,
+  ButtonShape,
+  ButtonSize,
+} from './Button.types';
 
-const getSize = (size: ButtonSize) => {
+const getSizeX = (size: ButtonSize) => {
+  switch (size) {
+    case 'xs':
+      return 4;
+    case 'sm':
+      return 4;
+    case 'md':
+      return 6;
+    case 'lg':
+      return 6;
+    case 'xl':
+      return 6;
+  }
+};
+
+const getSizeY = (size: ButtonSize) => {
   switch (size) {
     case 'xs':
       return 2;
@@ -18,42 +38,46 @@ const getSize = (size: ButtonSize) => {
 
 export const getClasses = (
   size: ButtonSize,
-  variant?: ButtonVariant,
+  fill?: ButtonFill,
+  shape?: ButtonShape,
   color?: ButtonColor,
   classes?: string,
   disabled?: boolean,
 ) => {
   const buttonClasses = clsx(
+    'w-full sm:w-auto',
     'rounded',
     {
-      'bg-primary text-light': color === 'primary' && variant === 'solid',
-      'bg-secondary text-light': color === 'secondary' && variant === 'solid',
-      'bg-tertiary text-light': color === 'tertiary' && variant === 'solid',
-      'bg-light text-dark': color === 'light' && variant === 'solid',
-      'bg-dark text-light': color === 'dark' && variant === 'solid',
-      border: variant === 'outline',
-      'border-primary text-primary':
-        variant === 'outline' && color === 'primary',
+      'bg-primary text-light': color === 'primary' && fill === 'solid',
+      'bg-secondary text-light': color === 'secondary' && fill === 'solid',
+      'bg-tertiary text-light': color === 'tertiary' && fill === 'solid',
+      'bg-light text-dark': color === 'light' && fill === 'solid',
+      'bg-dark text-light': color === 'dark' && fill === 'solid',
+      border: fill === 'outline',
+      'border-primary text-primary': fill === 'outline' && color === 'primary',
       'border-secondary text-secondary':
-        variant === 'outline' && color === 'secondary',
+        fill === 'outline' && color === 'secondary',
       'border-tertiary text-tertiary':
-        variant === 'outline' && color === 'tertiary',
-      'border-light text-light': variant === 'outline' && color === 'light',
-      'border-dark text-dark': variant === 'outline' && color === 'dark',
-      'border-transparent': variant === 'clear',
-      'text-primary': variant !== 'solid' && color === 'primary',
-      'text-secondary': variant !== 'solid' && color === 'secondary',
-      'text-tertiary': variant !== 'solid' && color === 'tertiary',
-      'text-light': variant !== 'solid' && color === 'light',
-      'text-dark': variant !== 'solid' && color === 'dark',
-      'disabled opacity-50': disabled,
-      'hover:opacity-75': !disabled,
+        fill === 'outline' && color === 'tertiary',
+      'border-light text-light': fill === 'outline' && color === 'light',
+      'border-dark text-dark': fill === 'outline' && color === 'dark',
+      'border-transparent': fill === 'clear',
+      'text-primary': fill !== 'solid' && color === 'primary',
+      'text-secondary': fill !== 'solid' && color === 'secondary',
+      'text-tertiary': fill !== 'solid' && color === 'tertiary',
+      'text-light': fill !== 'solid' && color === 'light',
+      'text-dark': fill !== 'solid' && color === 'dark',
+      'disabled bg-opacity-50': disabled,
+      'hover:bg-opacity-75': !disabled,
+      'rounded-full': shape === 'round',
+      'rounded-none': shape === 'square',
     },
     `text-${size}`,
-    `px-${getSize(size)}`,
-    `py-${getSize(size)}`,
+    `px-${getSizeX(size)}`,
+    `py-${getSizeY(size)}`,
     'font-medium',
     'border-2',
+    'flex items-center gap-16 justify-between',
     classes,
   );
 
