540b3b3627f18f149fc4c78e0d581a79863c39d1
Lucas Martinez <lsmartinez88@hotmail.com>
2024-01-12 11:49:47 -0300

    removing whitespaces

--- apps/web/app/[lang]/blog/components/blogCover/BlogCoverSkeleton.tsx
+++ apps/web/app/[lang]/blog/components/blogCover/BlogCoverSkeleton.tsx
@@ -3,7 +3,7 @@ import { BlogItemSkeleton } from '@components/shared/blogItem';
 export const BlogCoverSkeleton = () => {
   return (
     <>
-      <div className="col-span-12  mt-6 mb-6 sm:mt-10 sm:mb-10 border-b-1 border-gray-medium sm:border-0">
+      <div className="col-span-12 mt-6 mb-6 sm:mt-10 sm:mb-10 border-b-1 border-gray-medium sm:border-0">
         <div>
           <BlogItemSkeleton size="xl" position="end" orientation="vertical" />
         </div>
@@ -14,10 +14,10 @@ export const BlogCoverSkeleton = () => {
             <div className="border-b border-gray-medium pb-4 mt-4">
               <BlogItemSkeleton size="md" />
             </div>
-            <div className=" border-b border-gray-medium pb-4 mt-4">
+            <div className="border-b border-gray-medium pb-4 mt-4">
               <BlogItemSkeleton size="md" />
             </div>
-            <div className="  border-b border-gray-medium pb-4 mt-4">
+            <div className="border-b border-gray-medium pb-4 mt-4">
               <BlogItemSkeleton size="md" />
             </div>
             <div className="pb-4 mt-4">
@@ -27,7 +27,7 @@ export const BlogCoverSkeleton = () => {
         </div>
         <div className="sm:basis-1/3 order-1 sm:order-2">
           <div className="flex flex-col">
-            <div className=" border-b border-gray-medium mb-4 pb-4 sm:pb-10">
+            <div className="border-b border-gray-medium mb-4 pb-4 sm:pb-10">
               <BlogItemSkeleton orientation="vertical" size="md" />
             </div>
             <div className="sm:border-0 border-b border-gray-medium mb-4 pb-4 sm:pb-10">
