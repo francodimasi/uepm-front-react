0849fdc0f64eadeb12b06d718f4aeaba1fc9f8e9
Lucas <libattistella@gmail.com>
2024-01-18 22:04:07 +0100

    prettier

--- packages/ui/core/typography/H4.tsx
+++ packages/ui/core/typography/H4.tsx
@@ -1,7 +1,11 @@
 import clsx from 'clsx';
 import { TypographyProps } from './types';
 
-export const H4: React.FC<TypographyProps> = ({ label, children, className }) => {
+export const H4: React.FC<TypographyProps> = ({
+  label,
+  children,
+  className,
+}) => {
   const font = 'font-["Lexend"] font-semibold text-dark';
   const sizing = 'text-2xl leading-8';
   const spacing = 'my-2 pb-2 lg:my-3 lg:pb-3';
