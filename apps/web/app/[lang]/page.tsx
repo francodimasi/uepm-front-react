9e14eca9b22a861514c25e617f37c5040faa4551
Javier Bianciotto <javierbianciotto@gmail.com>
2024-01-19 11:23:42 -0300

    removed lang translation in home

--- apps/web/app/[lang]/page.tsx
+++ apps/web/app/[lang]/page.tsx
@@ -1,16 +1,11 @@
 import { Layout } from '@components/core/layout/Layout';
-import {
-  defaultLocale,
-  unstable_setRequestLocale,
-  getTranslations,
-} from 'intl';
+import { defaultLocale, unstable_setRequestLocale } from 'intl';
 import { promises as fs } from 'fs';
 import { Step } from './howitworks/HowItWorks.types';
 import { HowItWorks } from './howitworks';
 
 export default async function Page({ params: { lang = defaultLocale } }) {
   unstable_setRequestLocale(lang);
-  const t = await getTranslations('home');
 
   //TODO: replace with real fetch
   const getHowItWorksSteps = async () => {
@@ -26,8 +21,6 @@ export default async function Page({ params: { lang = defaultLocale } }) {
   return (
     <Layout locale={lang}>
       <HowItWorks steps={steps[lang]} />
-      <span>{`Language is: ${t('lang')}`}</span>
-      <br></br>
       <span>{`Locale is: ${lang}`}</span>
       <br></br>
       <br></br>
