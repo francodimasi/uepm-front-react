f9bbf8dd4956910815d48a5850cf6fe95be90a22
Lucas <libattistella@gmail.com>
2024-02-05 15:38:47 -0300

    prettier

--- packages/ui/components/algolia/hits/TextHit.tsx
+++ packages/ui/components/algolia/hits/TextHit.tsx
@@ -7,7 +7,7 @@ export const TextHit = ({ hit }: TextHitProps) => {
     <>
       {hit.object_type === STUDY && (
         <div className="flex items-center gap-1">
-          <div className='flex flex-col p-2'>
+          <div className="flex flex-col p-2">
             <P1>{hit.brief_title}</P1>
             <P2>{hit.nct_id}</P2>
             <P2>{hit.conditions_ct[0]}</P2>
@@ -16,7 +16,7 @@ export const TextHit = ({ hit }: TextHitProps) => {
       )}
       {hit.object_type === SITE && (
         <div className="">
-          <div className='flex flex-col p-2'>
+          <div className="flex flex-col p-2">
             <P1>{hit.name}</P1>
           </div>
         </div>
