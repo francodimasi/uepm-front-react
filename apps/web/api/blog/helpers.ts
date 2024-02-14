2aeab4bef3eee9858c3f1679d002a7a49f3900e6
Lucas Martinez <lsmartinez88@hotmail.com>
2024-02-14 12:02:06 -0300

    styling code

--- apps/web/api/blog/helpers.ts
+++ apps/web/api/blog/helpers.ts
@@ -1,7 +1,7 @@
 import { BlogArticle, BlogItem } from '@models/blog.types';
 import { BLOG } from './constants';
 
-const TITLE_TRUNCATE_LENGTH = 90
+const TITLE_TRUNCATE_LENGTH = 90;
 
 export const parseArticleItem = (article: BlogArticle): BlogItem => {
   const { title, date, featured_image_src, slug, category, tags } = article;
@@ -13,7 +13,10 @@ export const parseArticleItem = (article: BlogArticle): BlogItem => {
     date,
     image: featured_image_src,
     slug,
-    title: (title.rendered.length > TITLE_TRUNCATE_LENGTH) ? title.rendered.slice(0,TITLE_TRUNCATE_LENGTH) + '...' : title.rendered , 
+    title:
+      title.rendered.length > TITLE_TRUNCATE_LENGTH
+        ? title.rendered.slice(0, TITLE_TRUNCATE_LENGTH) + '...'
+        : title.rendered,
     tags,
   };
 
