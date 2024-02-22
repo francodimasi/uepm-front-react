c8ac536e92c45567e42e3da6d879fae4b357c481
Lucas Martinez <lsmartinez88@hotmail.com>
2024-02-22 13:37:26 -0300

    styling

--- packages/ui/core/radio-group/RadioGroup.tsx
+++ packages/ui/core/radio-group/RadioGroup.tsx
@@ -11,43 +11,47 @@ export const RadioGroup = ({
   name = 'radio',
   circleClassName = 'border-gray-300 text-indigo-600 focus:ring-indigo-600',
   titleClassName = 'text-sm font-medium text-gray-900',
-  descriptionClassName = 'text-sm text-gray-500'
+  descriptionClassName = 'text-sm text-gray-500',
 }: RadioGroupProps) => {
-
   const inputClass = 'h-4 w-4';
 
   return (
     <div className="space-y-5">
-        {items.map((item) => (
-          <div key={item.id} className="relative flex items-start">
-            <div className="flex h-6 items-center">
-              <input
-                id={item.id.toString()}
-                aria-describedby={`${item.id}-description`}
-                name={name}
-                type="radio"
-                defaultChecked={item.id === selected}
-                className= {twMerge(`${inputClass} ${circleClassName}`)}
-                onChange={(e) => onChange(e.target.value)}
-              />
-            </div>
-            <div className="ml-3 leading-6">
-              <label htmlFor={item.id.toString()} className={titleClassName}>
-                {item.title}
-              </label>
-              {radioDisposition === 'vertical' ? (
-                <p id={`${item.id.toString()}-description`} className={descriptionClassName}>
-                  {item.description}
-                </p>
-              ) : (
-                <span id={`${item.id.toString()}-description`} className={descriptionClassName}>
-                  {item.description}
-                </span>
-              )}
-              
-            </div>
+      {items.map((item) => (
+        <div key={item.id} className="relative flex items-start">
+          <div className="flex h-6 items-center">
+            <input
+              id={item.id.toString()}
+              aria-describedby={`${item.id}-description`}
+              name={name}
+              type="radio"
+              defaultChecked={item.id === selected}
+              className={twMerge(`${inputClass} ${circleClassName}`)}
+              onChange={(e) => onChange(e.target.value)}
+            />
           </div>
-        ))}
-      </div>
+          <div className="ml-3 leading-6">
+            <label htmlFor={item.id.toString()} className={titleClassName}>
+              {item.title}
+            </label>
+            {radioDisposition === 'vertical' ? (
+              <p
+                id={`${item.id.toString()}-description`}
+                className={descriptionClassName}
+              >
+                {item.description}
+              </p>
+            ) : (
+              <span
+                id={`${item.id.toString()}-description`}
+                className={descriptionClassName}
+              >
+                {item.description}
+              </span>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
