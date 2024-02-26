5caf9025966ac19b1eb38d4338a4d1a9e6643553
Javier Bianciotto <javierbianciotto@gmail.com>
2024-02-26 16:28:27 -0300

    Fix radio button alignments Target blank on site link

--- apps/web/app/[lang]/sites/[id]/components/SiteCard.tsx
+++ apps/web/app/[lang]/sites/[id]/components/SiteCard.tsx
@@ -35,7 +35,7 @@ export const SiteCard: React.FC<SiteCardProps> = ({
         {website ? (
           <div className="flex flex-col gap-2">
             <P2 className="text-base font-bold !m-0 !p-0">{t('website')}</P2>
-            <Link href={website}>
+            <Link href={website} target="_blank">
               <L1 className="text-base font-normal underline" label={website} />
             </Link>
           </div>
@@ -50,17 +50,9 @@ export const SiteCard: React.FC<SiteCardProps> = ({
           items={contactOptions}
           disposition="inline"
           orientation="horizontal"
+          titleClassName="ml-4"
         />
-        {/* <div className="flex justify-evenly gap-3">
-          <label>
-            <input type="radio" />
-            <P2 className="font-medium text-base ml-2">Soy Medico</P2>
-          </label>
-          <label>
-            <input type="radio" />
-            <P2 className="font-medium text-base ml-2">Soy Sponsor</P2>
-          </label>
-        </div> */}
+
         <ContactSiteButton text={t('contact')} />
       </div>
     </div>
